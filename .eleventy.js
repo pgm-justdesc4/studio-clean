985244a4d8c40f714f79ab76807b3d2b7e1ea896
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2024-02-27 16:29:02 +0100

    wip block-layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("input/assets/main.css");
+  eleventyConfig.addPassthroughCopy("input/assets/css/reset.css");
+  eleventyConfig.addPassthroughCopy("input/assets/css/main.css");
   eleventyConfig.addPassthroughCopy("input/assets/images");
 
   return {
